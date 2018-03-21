d3387b292f5915016f9fde9ab50056d5e38b6297
Danilo Cândido <danilod07@gmail.com>
2018-03-21 09:43:04 -0300

    redirect_to

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -7,4 +7,14 @@ class DemoController < ApplicationController
 
   def hello
   end
+
+  def other_hello
+    #redirect_to :root
+    #redirect_to :demo_hello
+    redirect_to(action: :index)
+  end
+
+  def google
+    redirect_to('http://google.com.br')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   #Simple match route
   #get 'demo/index' 
   get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/google'
 
   #Default Route, may go away in future rails versions
   #get ':controller(/:action(/:id))'
