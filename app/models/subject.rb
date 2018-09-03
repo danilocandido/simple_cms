fe39162e64f3ad1d44470a64691638ea6ba23304
Danilo Cândido <danilod07@gmail.com>
2018-09-03 13:26:08 -0300

    creating subject scopes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,6 @@
 class Subject < ApplicationRecord
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => true) }
+  scope :newest_first, lambda { order('created_at DESC') }
+  scope :search, lambda {|query| where(['name LIKE ?', "%#{query}%"])}
 end
